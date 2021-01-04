ae67ec7b8ada851e3452693ed240d60d91ea114b
ltwebb <loritwebb@gmail.com>
2021-01-03 19:53:05 -0500

    File Removal

--- public/js/time.js
+++
@@ -1,9 +0,0 @@
-var time = new Date().getHours();
-
-if (time > 18) {
-	document.getElementById("particles-js").style.backgroundImage = 'url(../images/night-tree-bg.jpg)';
-
-
-} else {
-document.getElementById("particles-js").style.backgroundImage = 'url(../images/day-tree-bg.jpg)';
-}
